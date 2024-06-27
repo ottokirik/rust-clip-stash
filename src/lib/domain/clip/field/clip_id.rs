38b197b55e62d0748f952413fc9a403d88327edd
Petrov Kirill <ottokirik@gmail.com>
2024-06-28 01:35:21 +0300

    feat: domain error enum

--- src/lib/domain/clip/field/clip_id.rs
+++ src/lib/domain/clip/field/clip_id.rs
@@ -1,1 +1,1 @@
-pub struct ClipId(String);
+
--- src/lib/domain/clip/field/mod.rs
+++ src/lib/domain/clip/field/mod.rs
@@ -1,16 +1,23 @@
 mod clip_id;
 pub use clip_id::ClipId;
+
 mod short_code;
 pub use short_code::ShortCode;
+
 mod content;
 pub use content::Content;
+
 mod title;
 pub use title::Title;
+
 mod posted_at;
 pub use posted_at::PostedAt;
+
 mod expires_at;
 pub use expires_at::ExpiresAt;
+
 mod password;
 pub use password::Password;
+
 mod hits;
 pub use hits::Hits;
