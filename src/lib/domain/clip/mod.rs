6d26f1c0bf1e2d087235e650e7fb57db4b880b83
Petrov Kirill <ottokirik@gmail.com>
2024-06-28 01:19:49 +0300

    feat: field file structure

--- src/lib/domain/clip/mod.rs
+++ src/lib/domain/clip/mod.rs
@@ -1,1 +1,13 @@
 pub mod field;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Clip {
+    pub clip_id: field::ClipId,
+    pub short_code: field::ShortCode,
+    pub content: field::Content,
+    pub title: field::Title,
+    pub posted_at: field::PostedAt,
+    pub expires_at: field::ExpiresAt,
+    pub password: field::Password,
+    pub hits: field::Hits,
+}
