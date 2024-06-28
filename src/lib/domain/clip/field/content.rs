2fee5f8a7b4b41dac758941c7136c5e12edca400
Petrov Kirill <ottokirik@gmail.com>
2024-06-28 11:25:27 +0300

    feat: fileds content, hits

---
+++ src/lib/domain/clip/field/content.rs
@@ -0,0 +1,22 @@
+use crate::domain::clip::ClipError;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Content(String);
+
+impl Content {
+    pub fn new(content: &str) -> Result<Self, ClipError> {
+        match content.trim().is_empty() {
+            true => Err(ClipError::EmptyContent),
+            false => Ok(Content(content.to_owned())),
+        }
+    }
+
+    pub fn into_inner(self) -> String {
+        self.0
+    }
+
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+}
