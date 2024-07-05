59ec114d95f40c7164164724c62faa013d59621e
Petrov Kirill <ottokirik@gmail.com>
2024-07-06 02:52:04 +0300

    feat: add api request guard

--- src/lib/web/api.rs
+++ src/lib/web/api.rs
@@ -1,10 +1,18 @@
 use std::str::FromStr;
 
 use base64::{engine::general_purpose::URL_SAFE, Engine as _};
-use rocket::{serde::json::Json, Responder};
+use rocket::{
+    http::Status,
+    request::{FromRequest, Outcome},
+    serde::json::Json,
+    Responder, State,
+};
 use serde::Serialize;
 
-use crate::service::ServiceError;
+use crate::{
+    data::AppDatabase,
+    service::{action, ServiceError},
+};
 
 pub const API_KEY_HEADER: &str = "x-api-key";
 
@@ -12,7 +20,7 @@ pub const API_KEY_HEADER: &str = "x-api-key";
 pub enum ApiKeyError {
     #[error("API key not found")]
     #[response(status = 404, content_type = "json")]
-    NoyFound(String),
+    NotFound(String),
 
     #[error("invalid API key format")]
     #[response(status = 400, content_type = "json")]
@@ -76,6 +84,47 @@ impl From<ServiceError> for ApiError {
             ServiceError::NotFound => Self::NotFound(Json("not found".to_owned())),
             ServiceError::Data(_) => Self::Server(Json("a server error occurred".to_owned())),
             ServiceError::PermissionError(err) => Self::User(Json(err)),
+        }
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ApiKey {
+    type Error = ApiError;
+
+    async fn from_request(req: &'r rocket::Request<'_>) -> Outcome<Self, Self::Error> {
+        fn server_error() -> Outcome<ApiKey, ApiError> {
+            Outcome::Error((
+                Status::InternalServerError,
+                ApiError::Server(Json("server error".to_owned())),
+            ))
+        }
+
+        fn key_error(err: ApiKeyError) -> Outcome<ApiKey, ApiError> {
+            Outcome::Error((Status::BadRequest, ApiError::KeyError(Json(err))))
+        }
+
+        match req.headers().get_one(API_KEY_HEADER) {
+            None => key_error(ApiKeyError::NotFound("API key not found".to_owned())),
+            Some(key) => {
+                let db = match req.guard::<&State<AppDatabase>>().await {
+                    Outcome::Success(db) => db,
+                    _ => return server_error(),
+                };
+
+                let api_key = match ApiKey::from_str(key) {
+                    Ok(key) => key,
+                    Err(err) => return key_error(err),
+                };
+
+                match action::is_valid_api_key(api_key.clone(), db.get_pool()).await {
+                    Ok(valid) if valid => Outcome::Success(api_key),
+                    Ok(valid) if !valid => {
+                        key_error(ApiKeyError::NotFound("API key not found".to_owned()))
+                    }
+                    _ => server_error(),
+                }
+            }
         }
     }
 }
